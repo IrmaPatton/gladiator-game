b357950ddb9a1796739838df0415a17172eb28c9
Irma <ipatton@basecampcodingacademy.org>
2018-07-09 16:27:52 -0500

    finished 1 example in test_core.py

--- test_core.py
+++ test_core.py
@@ -3,4 +3,10 @@ from core import *
 
 def test_new_gladiator():
     gladiator = new_gladiator(100, 10, 23, 46)
-    assert gladiator == {'health': 100, 'rage': 10, 'lowest damage': 23}
+    assert gladiator == {
+        'health': 100,
+        'rage': 10,
+        'lowest damage': 23,
+        'highest damage': 46
+    }
+    gladiator = # make another example
