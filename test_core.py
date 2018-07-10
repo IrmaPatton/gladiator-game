e7a27fd5e732e8abede10d8f47d4657a2885643c
Irma <ipatton@basecampcodingacademy.org>
2018-07-10 09:14:52 -0500

    finished the tests for new_gladiator in test_core.py

--- test_core.py
+++ test_core.py
@@ -2,11 +2,16 @@ from core import *
 
 
 def test_new_gladiator():
-    gladiator = new_gladiator(100, 10, 23, 46)
-    assert gladiator == {
+
+    assert new_gladiator(100, 10, 23, 46) == {
         'health': 100,
         'rage': 10,
         'lowest damage': 23,
         'highest damage': 46
     }
-    gladiator = # make another example
+    assert new_gladiator(0, 0, 0, 0) == {
+        'health': 0,
+        'rage': 0,
+        'lowest damage': 0,
+        'highest damage': 0
+    }
