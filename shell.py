55d4fba3bebe08dae6bcada891d3e48ea7f26559
Irma <ipatton@basecampcodingacademy.org>
2018-07-10 16:08:50 -0500

    made a greet_rule function in shell.py

--- shell.py
+++ shell.py
@@ -1,1 +1,7 @@
 from core import *
+
+
+def greet_rules():
+    print('''Welcome to Gladiators: Rage Beasts 
+It's a two player game, but if you want to play by yourself. I can't stop you
+                   May the power of rage guide you''')
