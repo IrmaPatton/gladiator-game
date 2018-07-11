3b8b6317dceca111db8f5741248d1b8428f670cf
Irma <ipatton@basecampcodingacademy.org>
2018-07-11 09:41:49 -0500

    made functions that make Player1 and Player2

--- shell.py
+++ shell.py
@@ -1,7 +1,18 @@
 from core import *
+from random import randint
 
 
 def greet_rules():
-    print('''Welcome to Gladiators: Rage Beasts 
+    print('''                   Welcome to Gladiators: Rage Beasts 
 It's a two player game, but if you want to play by yourself. I can't stop you
                    May the power of rage guide you''')
+
+
+def make_player_1():
+    Player1 = new_gladiator(100, 10, randint(10, 20), randint(21, 40))
+    return Player1
+
+
+def make_player_2():
+    Player2 = new_gladiator(100, 10, randint(10, 20), randint(21, 40))
+    return Player2
